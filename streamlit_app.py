b99788701e6d9f9e4b81db6b7cb6eb4bef76bb0a
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-11-06 07:59:01 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ if st.button("おみくじを引く"):
     }
     if user_name:
         st.write(f"{user_name}さんの結果:{result}")
-        st.write("{user_name}さんの結果:"+{result})
+        st.write({user_name}+"さんの結果:"+{result})
     else:
         st.write(f"結果:{result}")
     st.write(comments[result])
